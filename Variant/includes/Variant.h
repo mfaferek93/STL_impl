e5706008475571a56e82c33521ff7081cd20459e
Michal Faferek <michal.faferek.coe@gmail.com>
2022-05-21 14:24:35 +0200

    Initial commit for Variant type impl.

---
+++ Variant/includes/Variant.h
@@ -0,0 +1,86 @@
+#ifndef VARIANT
+#define VARIANT
+
+#include <cstddef>   // size_t
+#include <exception> // exception
+#include <typeindex> // type_index
+
+template <size_t val1, size_t... values>
+struct MaxSize;
+
+template <size_t val1, size_t val2, size_t... values>
+struct MaxSize<val1, val2, values...>
+{
+    static const size_t value = val1 > val2 ? MaxSize<val1, values...>::value
+                                            : MaxSize<val2, values...>::value;
+};
+
+template <size_t val>
+struct MaxSize<val>
+{
+    static const size_t value = val;
+};
+
+template <typename... Ts>
+struct Deleter;
+
+template <typename T, typename... Ts>
+struct Deleter<T, Ts...>
+{
+    static void Destroy(std::type_index id, char *data)
+    {
+        if (id == typeid(T))
+        {
+            reinterpret_cast<T *>(data)->~T();
+        }
+        else
+        {
+            Deleter<Ts...>::Destroy(id, data);
+        }
+    }
+};
+
+template <>
+struct Deleter<>
+{
+    static void Destroy(std::type_index id, char *data) {}
+};
+
+template <typename... Ts>
+class Variant
+{
+private:
+    static const size_t m_maxSize = MaxSize<sizeof(Ts)...>::value;
+
+public:
+    template <typename T>
+    Variant(const T &val)
+        : m_typeId(typeid(T))
+    {
+        new (&m_data) T(val);
+    }
+
+    template <typename T>
+    T &get()
+    {
+        if (m_typeId == typeid(T))
+        {
+            return *reinterpret_cast<T *>(m_data);
+        }
+        else
+        {
+            throw std::exception();
+        }
+    }
+
+    ~Variant()
+    {
+        Deleter<Ts...>::Destroy(m_typeId, m_data);
+    }
+
+private:
+    std::type_index m_typeId;
+    char m_data[m_maxSize];
+};
+
+#endif
---
+++ Variant/tests/VariantTest.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+
+#include "Variant.h"
+
+TEST(VariantTest, getMethod)
+{
+    Variant<int, float, bool, char, double> v(5);
+
+    EXPECT_NO_THROW(v.get<int>());
+    EXPECT_EQ(5, v.get<int>());
+
+    EXPECT_THROW(v.get<float>(), std::exception);
+    EXPECT_THROW(v.get<bool>(), std::exception);
+    EXPECT_THROW(v.get<char>(), std::exception);
+    EXPECT_THROW(v.get<double>(), std::exception);
+}
+
+class A
+{
+public:
+    ~A() { std::cout << "A" << std::endl; }
+};
+
+TEST(VariantTest, destructor)
+{
+    Variant<A, int> v(A{});
+    EXPECT_NO_THROW(v.get<A>());
+    EXPECT_THROW(v.get<int>(), std::exception);
+}
